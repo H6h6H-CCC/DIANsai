02a340f8b32b703ef6c6036b180992885cb3704b
HHH <abcd18237173091@163.com>
2026-05-28 21:14:30 +0800

    4

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -273,7 +273,7 @@ int main(void)
       //HAL_Delay(1000);
       HAL_Delay(1000);
       HAL_Delay(1000);
-      HAL_Delay(1000);
+      HAL_Delay(00);
       //State_RunCurrent();
       //Main_UpdateOledStatus();
       //__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, 2500);HAL_Delay(1000);
