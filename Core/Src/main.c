cd3399f4b5997f1e70aac977bed1987e772e8ffc
HHH <abcd18237173091@163.com>
2026-05-28 20:59:00 +0800

    3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -270,7 +270,7 @@ int main(void)
 	  while (1)
 	  {
       ///Adc3UartReport_Send();
-      HAL_Delay(1000);
+      //HAL_Delay(1000);
       HAL_Delay(1000);
       HAL_Delay(1000);
       HAL_Delay(1000);
