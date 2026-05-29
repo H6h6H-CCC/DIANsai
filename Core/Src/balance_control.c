ff09d78907d7b45e2d71d5b7b17a69d68f1c77b7
HHH <abcd18237173091@163.com>
2026-05-30 02:15:43 +0800

    22222

--- Core/Src/balance_control.c
+++ Core/Src/balance_control.c
@@ -1,5 +1,6 @@
 #include "balance_control.h"
 #include "angle_sensor.h"
+#include "encoder.h"
 #include "moter.h"
 #include "pid.h"
 
@@ -8,9 +9,13 @@
 #define BALANCE_I_LIMIT       100.0f
 #define BALANCE_MIN_ANGLE     151.5f
 #define BALANCE_MAX_ANGLE     179.5f
+#define BALANCE_SPEED_ANGLE_LIMIT 1.3f
+#define BALANCE_SPEED_I_LIMIT 30.0f
 
 static PID_t s_angle_pid;
+static PID_t s_speed_pid;
 static float s_target_angle = 165.0f;
+static float s_target_speed = 0.0f;
 static uint8_t s_enable = 1U;
 static volatile int16_t s_last_pwm = 0;
 
@@ -26,14 +31,19 @@ static float Balance_AngleError(float target, float measure)
 
 void Balance_Init(void)
 {
-    PID_Init(&s_angle_pid, 230.0f, 0.5f, 10.0f, BALANCE_PWM_LIMIT, BALANCE_I_LIMIT);
-    s_target_angle = 164.0f;
+    PID_Init(&s_angle_pid, 235.0f, 0.7f, 11.0f, BALANCE_PWM_LIMIT, BALANCE_I_LIMIT);
+    PID_Init(&s_speed_pid, 0.00115f, 0.0f, 0.0f, BALANCE_SPEED_ANGLE_LIMIT, BALANCE_SPEED_I_LIMIT);
+    s_target_angle = 163.8f;
+    s_target_speed = 0.0f;
     s_enable = 1U;
 }
 
 void Balance_Update10ms(void)
 {
     float angle;
+    float target_angle;
+    float speed;
+    float angle_offset;
     float error;
     float abs_error;
     int16_t pwm;
@@ -43,11 +53,15 @@ void Balance_Update10ms(void)
         Moter_B(0);
         s_last_pwm = 0;
         PID_Reset(&s_angle_pid);
+        PID_Reset(&s_speed_pid);
         return;
     }
 
     angle = AngleSensor_GetAngle();
-    error = Balance_AngleError(s_target_angle, angle);
+    speed = ((float)Encoder3_GetLastDelta() + (float)Encoder4_GetLastDelta()) * 0.5f;
+    angle_offset = PID_Update(&s_speed_pid, s_target_speed, speed, BALANCE_DT_S);
+    target_angle = s_target_angle + angle_offset;
+    error = Balance_AngleError(target_angle, angle);
     abs_error = (error < 0.0f) ? -error : error;
 
     if ((angle < BALANCE_MIN_ANGLE) || (angle > BALANCE_MAX_ANGLE)) {
@@ -55,6 +69,7 @@ void Balance_Update10ms(void)
         Moter_B(0);
         s_last_pwm = 0;
         PID_Reset(&s_angle_pid);
+        PID_Reset(&s_speed_pid);
         return;
     }
 
@@ -83,6 +98,7 @@ void Balance_Enable(uint8_t enable)
         Moter_B(0);
         s_last_pwm = 0;
         PID_Reset(&s_angle_pid);
+        PID_Reset(&s_speed_pid);
     }
 }
 
